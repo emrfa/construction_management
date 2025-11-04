9041eb49c3949aeaff7cbee3d6cb14da9a318fe7
emrfa <muhammademirfaisa@gmail.com>
2025-11-04 14:38:39 +0700

    Fixed Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->firstOrCreate(
+            ['email' => 'test@example.com'],
+            ['name' => 'Test User']
+        );
         $this->call(RolesAndPermissionsSeeder::class);
     }
 }
